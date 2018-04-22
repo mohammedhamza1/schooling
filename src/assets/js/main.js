4ffce42d891097c8380afaa280fbedd884358ec9
Mohammed Hamza <medomohammed850@gmail.com>
2018-04-22 15:26:23 +0200

    simple fix in home slider component.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
+    $.fn.centerany = function(){
+        $(this).css({
+            'position':'absolute',
+            'left': ($(this).parent().width()-$(this).width())/2,
+            'top': ($(this).parent().height()-$(this).height())/2
+        });
+    }
+
+    $('.info-wrapper .sinfo').centerany();
+
     $('.alert').alert();
     //Smooth-Scrolling 
     $(".scroll").click(function (event) {
