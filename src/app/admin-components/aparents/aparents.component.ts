9bd3860d816d4c3a60d0977f6b70b8b315a4d5e9
Mohammed Hamza <medomohammed850@gmail.com>
2018-06-30 10:53:18 +0200

    Added aparent component.

---
+++ src/app/admin-components/aparents/aparents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aparents',
+  templateUrl: './aparents.component.html',
+  styleUrls: ['./aparents.component.css']
+})
+export class AparentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
