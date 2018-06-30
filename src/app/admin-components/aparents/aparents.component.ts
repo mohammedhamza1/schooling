3cbc6aeee2b9ac4b9c0271240ff13099e421cc49
Mohammed Hamza <medomohammed850@gmail.com>
2018-06-30 14:06:38 +0200

    Added parents to contact with.

--- src/app/admin-components/aparents/aparents.component.ts
+++ src/app/admin-components/aparents/aparents.component.ts
@@ -6,15 +6,19 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./aparents.component.css']
 })
 export class AparentsComponent implements OnInit {
-    items: any;
-    term: any;
+    parentsArray: any;
+    term: string;
 
     constructor() {
     }
 
     ngOnInit() {
-        this.items = [
-            {name: 'archie'}, {name: 'jake'}, {name: 'richard'}
+        this.parentsArray = [
+            {id: 1, fname: 'hamza', lname: 'makram'},
+            {id: 2, fname: 'ahmed', lname: 'ali'},
+            {id: 3, fname: 'mahmoud', lname: 'hegazi'},
+            {id: 4, fname: 'eslam', lname: 'ali'},
+            {id: 5, fname: 'adel', lname: 'amer'},
         ];
     }
 
