8e65edb7de35384d272e19943495cc24395a1ffa
Mohammed Hamza <medomohammed850@gmail.com>
2018-07-04 18:52:56 +0200

    Editing admin classes component.

--- src/app/admin-components/staff/classes/classes.component.ts
+++ src/app/admin-components/staff/classes/classes.component.ts
@@ -8,7 +8,6 @@ import {Component, OnInit} from '@angular/core';
 export class ClassesComponent implements OnInit {
     p: number;
     classesArray: any;
-    studentsArray: any;
 
     constructor() {
     }
@@ -16,24 +15,90 @@ export class ClassesComponent implements OnInit {
     ngOnInit() {
         this.p = 1;
         this.classesArray = [
-            {id: 1, name: 'b-2', level: '4', studentsNum: 30},
-            {id: 2, name: 'a-4', level: '5', studentsNum: 20},
-            {id: 3, name: 'c-6', level: '6', studentsNum: 40},
-            {id: 4, name: 'd-1', level: '3', studentsNum: 35}
-        ];
-
-        this.studentsArray = [
-            {id: 1, fname: 'mohammed', lname: 'hamza', class: 'b-2'},
-            {id: 2, fname: 'ahmed', lname: 'ali', class: 'b-2'},
-            {id: 3, fname: 'ali', lname: 'hamza', class: 'b-2'},
-            {id: 4, fname: 'hassan', lname: 'mohammed', class: 'b-2'},
-            {id: 5, fname: 'nour', lname: 'elbana', class: 'b-2'},
-            {id: 6, fname: 'sayed', lname: 'mohamady', class: 'b-2'},
-            {id: 7, fname: 'karim', lname: 'ashraf', class: 'b-2'},
-            {id: 8, fname: 'ibrahem', lname: 'adel', class: 'b-2'},
-            {id: 9, fname: 'salah', lname: 'hegazi', class: 'b-2'},
-            {id: 10, fname: 'amr', lname: 'nour', class: 'b-2'},
-            {id: 11, fname: 'khaled', lname: 'ali', class: 'b-2'}
+            {
+                id: 1,
+                name: 'b-4',
+                level: '4',
+                classStudents: [
+                    {id: 1, fname: 'mohammed', lname: 'hamza', class: 'b-2'},
+                    {id: 2, fname: 'ahmed', lname: 'ali', class: 'b-2'},
+                    {id: 3, fname: 'ali', lname: 'hamza', class: 'b-2'},
+                    {id: 4, fname: 'hassan', lname: 'mohammed', class: 'b-2'},
+                    {id: 5, fname: 'nour', lname: 'elbana', class: 'b-2'},
+                    {id: 6, fname: 'sayed', lname: 'mohamady', class: 'b-2'},
+                    {id: 7, fname: 'karim', lname: 'ashraf', class: 'b-2'},
+                    {id: 8, fname: 'ibrahem', lname: 'adel', class: 'b-2'},
+                    {id: 9, fname: 'salah', lname: 'hegazi', class: 'b-2'},
+                    {id: 10, fname: 'amr', lname: 'nour', class: 'b-2'},
+                    {id: 11, fname: 'khaled', lname: 'ali', class: 'b-2'},
+                    {id: 12, fname: 'ibrahem', lname: 'adel', class: 'b-2'},
+                    {id: 13, fname: 'salah', lname: 'hegazi', class: 'b-2'},
+                    {id: 14, fname: 'amr', lname: 'nour', class: 'b-2'},
+                    {id: 15, fname: 'fawzy', lname: 'magdy', class: 'b-2'},
+                    {id: 16, fname: 'hani', lname: 'eissa', class: 'b-2'},
+                    {id: 17, fname: 'magdy', lname: 'ebrahim', class: 'b-2'},
+                    {id: 18, fname: 'hassan', lname: 'mohammed', class: 'b-2'},
+                    {id: 19, fname: 'nour', lname: 'elbana', class: 'b-2'},
+                ]
+            },
+            {
+                id: 2,
+                name: 'c-2',
+                level: '5',
+                classStudents: [
+                    {id: 1, fname: 'fawzy', lname: 'magdy', class: 'b-2'},
+                    {id: 2, fname: 'hani', lname: 'eissa', class: 'b-2'},
+                    {id: 3, fname: 'magdy', lname: 'ebrahim', class: 'b-2'},
+                    {id: 4, fname: 'hassan', lname: 'mohammed', class: 'b-2'},
+                    {id: 5, fname: 'nour', lname: 'elbana', class: 'b-2'},
+                    {id: 6, fname: 'sayed', lname: 'mohamady', class: 'b-2'},
+                    {id: 7, fname: 'karim', lname: 'ashraf', class: 'b-2'},
+                    {id: 8, fname: 'ibrahem', lname: 'adel', class: 'b-2'},
+                    {id: 9, fname: 'salah', lname: 'hegazi', class: 'b-2'},
+                    {id: 10, fname: 'amr', lname: 'nour', class: 'b-2'},
+                    {id: 11, fname: 'khaled', lname: 'ali', class: 'b-2'}
+                ]
+            },
+            {
+                id: 3,
+                name: 'd-2',
+                level: '6',
+                classStudents: [
+                    {id: 1, fname: 'fawzy', lname: 'magdy', class: 'b-2'},
+                    {id: 2, fname: 'hani', lname: 'eissa', class: 'b-2'},
+                    {id: 3, fname: 'magdy', lname: 'ebrahim', class: 'b-2'},
+                    {id: 4, fname: 'hassan', lname: 'mohammed', class: 'b-2'},
+                    {id: 5, fname: 'nour', lname: 'elbana', class: 'b-2'},
+                    {id: 6, fname: 'sayed', lname: 'mohamady', class: 'b-2'},
+                    {id: 7, fname: 'karim', lname: 'ashraf', class: 'b-2'},
+                    {id: 8, fname: 'ibrahem', lname: 'adel', class: 'b-2'},
+                    {id: 9, fname: 'salah', lname: 'hegazi', class: 'b-2'},
+                    {id: 10, fname: 'amr', lname: 'nour', class: 'b-2'}
+                ]
+            },
+            {
+                id: 4,
+                name: 'a-6',
+                level: '2',
+                classStudents: [
+                    {id: 1, fname: 'fawzy', lname: 'magdy', class: 'b-2'},
+                    {id: 2, fname: 'hani', lname: 'eissa', class: 'b-2'},
+                    {id: 3, fname: 'magdy', lname: 'ebrahim', class: 'b-2'},
+                    {id: 4, fname: 'hassan', lname: 'mohammed', class: 'b-2'},
+                    {id: 5, fname: 'nour', lname: 'elbana', class: 'b-2'},
+                    {id: 6, fname: 'sayed', lname: 'mohamady', class: 'b-2'},
+                    {id: 7, fname: 'karim', lname: 'ashraf', class: 'b-2'},
+                    {id: 8, fname: 'ibrahem', lname: 'adel', class: 'b-2'},
+                    {id: 9, fname: 'salah', lname: 'hegazi', class: 'b-2'},
+                    {id: 10, fname: 'amr', lname: 'nour', class: 'b-2'},
+                    {id: 11, fname: 'khaled', lname: 'ali', class: 'b-2'},
+                    {id: 12, fname: 'nour', lname: 'elbana', class: 'b-2'},
+                    {id: 13, fname: 'sayed', lname: 'mohamady', class: 'b-2'},
+                    {id: 14, fname: 'karim', lname: 'ashraf', class: 'b-2'},
+                    {id: 15, fname: 'ibrahem', lname: 'adel', class: 'b-2'},
+                    {id: 16, fname: 'salah', lname: 'hegazi', class: 'b-2'}
+                ]
+            }
         ];
     }
 
